6bdc0a1a59ee66d9fde68d47048b9c5c8d3b5d68
Alex <alex.greenland@gmail.com>
2023-12-01 22:54:48 +0000

    updates

---
+++ hosts.repo.py
@@ -0,0 +1,42 @@
+PIHOSTS = {
+    'pi1': {
+        'name': 'Name 1',
+        'ip' : '192.168.1.1',
+        'user': 'pi',
+        'password': 'password',
+        'home': '/home/pi',
+        'bus': 1,
+        'address': 0x3c,
+        'thread': None
+    },
+    'pi2': {
+        'name': 'Name 2',
+        'ip' : '192.168.1.2',
+        'user': 'pi',
+        'password': 'password',
+        'home': '/home/pi',
+        'bus': 1,
+        'address': 0x3d,
+        'thread': None
+    },
+    'pi3': {
+        'name': 'Name 3',
+        'ip' : '192.168.1.3',
+        'user': 'pi',
+        'password': 'password',
+        'home': '/home/pi',
+        'bus': 6,
+        'address': 0x3c,
+        'thread': None
+    },
+    'pi4': {
+        'name': 'Name 4',
+        'ip' : '192.168.1.4',
+        'user': 'pi',
+        'password': 'password',
+        'home': '/home/pi',
+        'bus': 6,
+        'address': 0x3d,
+        'thread': None
+    }       
+}
